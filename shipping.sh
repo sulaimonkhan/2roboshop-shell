784a2b5f1dd9704fd829b36831f18d602a2d0706
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-13 19:58:17 +0530

    shipping update

--- shipping.sh
+++ shipping.sh
@@ -14,6 +14,12 @@ cd /app
 mvn clean package 
 mv target/shipping-1.0.jar shipping.jar 
 
+dnf install mysql -y
+
+mysql -h mysql.sulaimondevopsb72.online -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql.sulaimondevopsb72.online -uroot -pRoboShop@1 < /app/db/app-user.sql 
+mysql -h mysql.sulaimondevopsb72.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+
 systemctl daemon-reload
 systemctl enable shipping 
 systemctl restart shipping
