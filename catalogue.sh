d9d6cac99c3f5e01f407de6401eaa4bcac7f0319
sulaimonkhan <sulaimonkhan04@gmail.com>
2024-10-13 18:52:01 +0530

    2roboshop-shell shell script

---
+++ catalogue.sh
@@ -0,0 +1,24 @@
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+
+dnf install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+mongosh --host mongodb.sulaimondevopsb72.online </app/db/master-data.js
+
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl restart catalogue
